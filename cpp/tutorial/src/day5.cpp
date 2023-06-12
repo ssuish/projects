59727d4d33f9a5f19906522b43b305b7ebda7acf
suisoh <fubuki.k016@gmail.com>
2023-06-12 16:06:43 +0800

    return type functions

--- cpp/tutorial/src/day5.cpp
+++ cpp/tutorial/src/day5.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 
+// Example user-defined function, using void return type.
+// void return type doesn't need a return statement.
+void doPrint(){
+    std::cout << "in doPrint()\n";
+}
+
 int main(){
 
-    
+    /*
+        Functions
+
+        Follows the format: 
+        returnType functionName()
+        { 
+            function body 
+        }
+
+        Every executable program needs only one main function. It is the program that the compiler runs.
+        Every statement in the function are executed sequentially until it encouters a function call.
+        You can call functions more than once.
+        You can call functions inside a function.
+        Nested functions are not supported.
+
+        Function call tells the CPU to interrupt the current function and execute another function. It puts
+        a bookmark at current point of execution, and then calls the function named in the function call. 
+        When the function ends, the CPU returns back to the point it bookmarked, and resumes the execution.
+
+    */
+
+    std::cout << "Starting main()\n";
+    doPrint();
+    std::cout << "Ending main()\n";
 
     return 0;
 }
---
+++ cpp/tutorial/src/day5_2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+int getValueFromUser()
+{
+    std::cout << "Enter an Integer: ";
+    int input{ };
+    std::cin >> input;
+    return input;
+}
+
+int main(){
+    int num {getValueFromUser()};
+    std::cout << num << "doubled is: " << num * 2 << '\n';
+
+    // value-returning function that does not have a return type will produce undefined behavior.
+    // make sure all functions with non-void return types return a value in all cases.
+    // functions can only return a single value.
+
+    // functions can be used:
+    // 1. if you need to do something more than once.
+    // 2. used to define a sequence of statements we want to execute more than once.
+
+    return 0; // return value of main function is status code. 0 means successful. 
+}
+
+// Error: It's illegal to nest a function
+// int return7()
+// {
+//     return 7;
+
+//     int return9()
+//     {
+//         return 9;
+//     }
+// }
+
+// Error: You can only return one value
+// int getNumbers()
+// {
+//     return 5;
+//     return 7;
+// }

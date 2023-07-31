7c07763f37b497b806dde99d7f77d88862fe6e5c
suisoh <fubuki.k016@gmail.com>
2023-07-31 20:37:40 +0800

    problem 5

---
+++ problems/cpp_practice/permutation.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int P(int n, int r)
+{
+    int out = 0;
+
+    return out;
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    return 0;
+}

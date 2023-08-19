0d9f02d3c7f882643281321658bd443aaa5f30e0
suisoh <fubuki.k016@gmail.com>
2023-08-19 04:46:54 +0000

    attribute parser draft

---
+++ problems/cpp_hackerrank/attributeparser.cpp
@@ -0,0 +1,32 @@
+/*
+Strings - Hackerrank
+Difficulty: Medium
+Suggested Time Complexity: N/A
+Topics covered: String Streams, Strings
+Problem Link: https://www.hackerrank.com/challenges/attribute-parser
+
+Editorial
+
+*/
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n, q;
+    cin >> n >> q;
+
+    string[n] hrml = [];
+
+    if (int i = 0; i < n; ++n)
+    {
+        
+    }
+
+
+    return 0;
+}
